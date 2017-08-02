e6c26af214d2c98545a8b8689140eef07b3fe218
diego.delmiro <di3g0d0ming05@gmail.com>
2017-08-02 15:47:59 -0300

    criando dependencias

--- teste.py
+++ teste.py
@@ -1,21 +1,27 @@
 import pyautogui
 import time
+import sys
 
 
 def login():
-    pyautogui.typewrite("1", interval=1)
+    pyautogui.typewrite("1", interval=0.2)
     pyautogui.press('enter', interval=0.2)
     pyautogui.typewrite("ctr", interval=0.2)
     pyautogui.typewrite("043244", interval=0.2)
-    pyautogui.press('enter', interval=1)
+    pyautogui.press('enter', interval=0.2)
     i = 0
     while i < 3:
-        pyautogui.press('enter', interval=1)
+        pyautogui.press('enter', interval=0.2)
         i += 1
 
 
 def modulovendas():
     pyautogui.hotkey('Alt', 'v')
+    time.sleep(5)
+    pyautogui.keyDown('Alt')
+    pyautogui.press('s')
+    pyautogui.press('r')
+    pyautogui.keyUp('Alt')
 
 
 def modulocontareceber():
@@ -45,4 +51,9 @@ if __name__ == "__main__":
     time.sleep(2)
     login()
     time.sleep(2)
-    modulovendas()
+    if sys.argv[1] == "1":
+        modulovendas()
+    elif sys.argv[1] == "2":
+        modulolivrofiscal()
+    else:
+        print(u"opcão invalida")
